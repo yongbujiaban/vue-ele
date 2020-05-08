b594a507f006a1d8ec2774af34fd56fc27114f5b
yongbujiaban <946130228@qq.com>
2020-05-08 17:34:33 +0800

    '1'

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,16 @@
+<!--
+ * @Author: 🌝BackgroundColor🌝
+ * @Date: 2020-05-08 16:34:26
+ * @LastEditTime: 2020-05-08 16:54:33
+ * @Description: 😎佛祖保佑   🙏   永无BUG😎
+ -->
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    
     <router-view/>
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
